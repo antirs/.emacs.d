ded42fc55de97d5e85384b9856364ce278eec457
enomem <enomem@substation>
2025-11-10 10:31:10 +0300

    enomem:en-perl: recognize psgi files

--- site-lisp/enomem/en-perl.el
+++ site-lisp/enomem/en-perl.el
@@ -1,6 +1,6 @@
 ;;; perl-mode
 (use-package perl-mode
-  :mode ("\\(\\.cgi\\|\\.t\\|alienfile\\|repl.rc\\)\\'" . perl-mode))
+  :mode ("\\(\\.cgi\\|\\.psgi\\|\\.t\\|alienfile\\)\\'" . perl-mode))
 
 ;;; cperl-mode
 (use-package cperl-mode
