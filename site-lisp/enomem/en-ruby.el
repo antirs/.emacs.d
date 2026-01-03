18cac242447cf975d0c03b8539b9d70292905426
enomem <enomem@substation>
2026-01-03 23:46:13 +0300

    enomem:en-ruby: add rvm package

--- site-lisp/enomem/en-ruby.el
+++ site-lisp/enomem/en-ruby.el
@@ -2,6 +2,9 @@
 (use-package enh-ruby-mode :disabled
   :mode ("\\.rb\\'" . enh-ruby-mode))
 
+;;; rvm
+(use-package rvm)
+
 ;;; enomem/en-lsp-ruby
 (use-package enomem/en-lsp-ruby :load-path "site-lisp")
 
