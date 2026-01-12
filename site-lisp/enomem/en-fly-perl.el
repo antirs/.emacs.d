67d1a76842b78078d5ed2df5313ba812d78ff55f
enomem <enomem@substation>
2026-01-12 20:54:32 +0300

    enomem:en-perl: add raku flycheck and flymake support

--- site-lisp/enomem/en-fly-perl.el
+++ site-lisp/enomem/en-fly-perl.el
@@ -3,4 +3,14 @@
   :after flycheck
   :hook (perl-mode . flycheck-mode))
 
+;;; flycheck-raku
+(use-package flycheck-raku :load-path "site-lisp"
+  :after flycheck
+  :hook (raku-mode . flycheck-mode))
+
+;;; flymake-rakudo
+(use-package flymake-rakudo :load-path "site-lisp"
+  :after flymake
+  :hook (raku-mode . flymake-mode))
+
 (provide 'enomem/en-fly-perl)
